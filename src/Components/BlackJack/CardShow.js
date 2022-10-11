d7789aab71002e0ab3b42b4c67eb3ec9ffdf2f7e
Jimmy Ayivor <jimmyayivor@gmail.com>
2022-10-11 17:31:42 -0400

    update game  UI

--- src/Components/BlackJack/CardShow.js
+++ src/Components/BlackJack/CardShow.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Button, Card } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import tim from "../../Assets/tim.png"
 
 function CardShow({card}) {
   return (
-    <Card className="text-center">
+    <Card className="" style={{width:150 , height:200 , marginLeft: 10}}>
       <Card.Header>{card.face} {card.suit}</Card.Header>
       <Card.Body>
-        <Card.Img className="py-3" variant="top" src={tim} style={{width:"300px"}}
+        <Card.Img className="py-3" variant="top" src={tim} style={{width:"50px"}}
         />
         </Card.Body>
       <Card.Footer>{card.suit} {card.face} </Card.Footer>
